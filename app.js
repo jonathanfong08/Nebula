de2ac0d969f37de30868502d1e266f745f7f3b45
jonathanfong08 <122180696+jonathanfong08@users.noreply.github.com>
2023-04-06 14:42:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import * as uuid from "uuid";
 import fs from "node:fs";
 import bcrypt from "bcrypt";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 const __dirname = process.cwd();
 const ACTIVE_CODES = new Set();
 if (!fs.existsSync("./memory.txt")) {
